b646ce6c04b74614a13d02b712706817518dff71
Aanvi <aanvic527@gmail.com>
2026-07-10 14:25:06 +0530

    train

--- temp.py
+++ temp.py
@@ -1,16 +1,5 @@
-from ai.data.preprocessing.dataloader import PulmoDataset
-from ai.data.preprocessing.transforms import train_transform
+import torch
 
-dataset = PulmoDataset(
-    "ai/data/processed/training_dataset.csv",
-    transform=train_transform
-)
-
-image, metadata, labels = dataset[0]
-
-print(image.shape)
-print(metadata.shape)
-print(labels.shape)
-
-print(metadata)
-print(labels)
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
